675ebc4e39da374760c269b502e883a03576d472
unknown <ianchen6501@gmail.com>
2021-02-04 16:01:33 +0800

    index.js 修改排版

--- index.js
+++ index.js
@@ -13,13 +13,12 @@ const corsOptions = {
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
 app.use(cors(corsOptions)); //設定 cors，預設非 same-origin 不能存取
-// app.set('view engine', 'ejs') //設定 view enging
-
 //body parsor_middleware_可以解析 json / post 等資訊
 app.use(bodyParser.urlencoded({ extended: false })) //parse urlencoed
 app.use(bodyParser.json()) //parse json
 
-//connect-flash，可以回傳 flash 
+// app.set('view engine', 'ejs') //設定 view enging
+// connect-flash，可以回傳 flash 
 // app.use(flash())
 
 //用 res.locals 可以設定在 view 使用的變數
@@ -30,24 +29,24 @@ app.use(session({
   saveUninitialized: true,
 }))
 
-//users
+//usersController
 const usersController = require('./controlers/users')
 
 app.post('/users/', usersController.tokenLogin)
-app.post('/register/:method', usersController.register)
 app.post('/login/:method', usersController.login)
+app.post('/register/:method', usersController.register)
 
-//schedules
+//schedulesController
 const schedulesController = require("./controlers/schedules")
 app.get(`/schedules`, schedulesController.getAllSchedules)
-app.get(`/posts`, schedulesController.getAllPosts)
-app.get(`/posts/:scheduleId`, schedulesController.getOnePost)
 app.get(`/schedules/:userId`, schedulesController.getUserAllSchedules)
 app.get('/schedules/:userId/:id', schedulesController.getUserOneSchedule)
 app.post('/schedules/', schedulesController.addSchedule)
 app.delete('/schedules/:id', schedulesController.deleteSchedule)
 app.put('/schedules/:id', schedulesController.putSchedule)
 app.patch('/schedules/:id', schedulesController.patchScheduleIsFinished)
+app.get(`/posts`, schedulesController.getAllPosts)
+app.get(`/posts/:scheduleId`, schedulesController.getOnePost)
 
 app.listen(port, () => { //監聽 port
   console.log(`Example app listening at http://localhost:${port}`)
